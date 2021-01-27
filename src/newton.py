4e30b10721fdab6cf0562146f75710198c68c6b1
Nick Richardson <njkrichardson@princeton.edu>
2021-01-26 18:35:55 -0600

    continued work on #4

--- src/newton.py
+++ src/newton.py
@@ -15,45 +15,25 @@ def is_square(arr: np.ndarray) -> bool:
 
 def newton(function: callable, x_initial: np.ndarray) -> np.ndarray: 
     x_ = deepcopy(x_initial)
-    derivative = grad(function) 
+    update = get_newton_update(function, x_.size)
     for _ in range(kwargs.get("max_iterations", int(1e2))): 
-        update_ = function(x_)/derivative(x_)
+        update_ = update(x_)
         x_ -= update_ 
-        if norm(update) <= kwargs.get("convergence_tol", 1e-3): 
+        if norm(update_) <= kwargs.get("convergence_tol", 1e-3): 
             break 
     return x_ 
 
-
-
-
-
-    
-
-def newton_rhapson(function: callable, x_0: np.ndarray, **kwargs) -> np.ndarray: 
-    import pdb; pdb.set_trace()
-    x_estimated = deepcopy(x_0)
-
-    if x_0.size == 1: 
-        derivative = grad(function) 
-        _newton_update = lambda x: function(x)/derivative(x)
-    elif x_0.size > 1: 
-        jacobian = jacfwd(function) 
-
+def get_newton_update(function: callable, output_dimension: int) -> callable: 
+    if output_dimension > 1: 
         if is_tall(jacobian) is True: 
             def _newton_update(x: np.ndarray) -> np.ndarray: 
                 jacobian_psuedo_inverse = solve(jacobian(x) @ jacobian(x), jacobian(x).T)
                 return jacobian_psuedo_inverse @ function(x) 
-
+            return _newton_update
         else: 
-            _newton_update = lambda x: solve(jacobian(x), -function(x)) + x 
-
-    for _ in range(kwargs.get("maximum_interations", 100)): 
-        _update = _newton_update(x_estimated) 
-        x_estimated -= _update 
-        if norm(_update) < kwargs.get("covergence_tol", 1e-3): 
-            break 
-
-    return x_estimated 
+            return lambda x: solve(jacobian(x), -function(x)) + x 
+    else: 
+        return lambda x: function(x)/grad(function)(x)
 
 if __name__=="__main__": 
 
